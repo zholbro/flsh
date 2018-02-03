4c56a09eb2d8ab27f5e3af4e0eae911b58f30a7d
zach holbrook <zholbroo@ucsc.edu>
2018-02-03 12:20:34 -0800

    Refactor and no more server crashes

---
+++ back/models.py
@@ -0,0 +1,57 @@
+"""
+	models.py
+	Generic and specific models for FLSH's SQLite database
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+from serv import db
+
+# Bathroom model
+# Columns are very evident, a specific address and floor are optional
+# Variables like 'cleanliness' and 'quality' are calculated as the average
+
+class Bathroom(db.Model):
+	id = db.Column(db.Integer, primary_key = True, nullable = False)
+	building = db.Column(db.String(20), nullable = False)
+	address = db.Column(db.String(80))
+	floor = db.Column(db.Integer)
+	gender = db.Column(db.String(10), nullable = False)
+	cleanliness = db.Column(db.Float, nullable = False)
+	quality = db.Column(db.Float, nullable = False)
+	latitude = db.Column(db.Float, nullable = False)
+	longitude = db.Column(db.Float, nullable = False)
+
+
+# Generic model
+# for generic items in the DB
+# they all have location data, and will have generic reviews attached to them
+
+class Generic(db.Model):
+	id = db.Column(db.Integer, primary_key = True, nullable = False)
+	name = db.Column(db.String(20), nullable = False)
+	floor = db.Column(db.Integer)
+	building = db.Column(db.String(30), nullable = False)
+	address = db.Column(db.String(80))
+	rating = db.Column(db.Integer)
+
+
+# Per-item review counter
+# We have this so the individual model for Bathroom doesn't get ridiculous
+# When updating the general bathroom metadata, knowing how many times
+# a bathroom has been reviewed so we can take the average of multiple
+# reviews
+
+class ReviewCount(db.Model):
+	id = db.Column(db.Integer, primary_key = True, nullable = False)
+	ItemID = db.Column(db.Integer, nullable = False)
+	count = db.Column(db.Integer, nullable = False)
+
+# DB entry for Bathroom reviews
+# Specific reviews are pulled by querying the id
+# id per review corresponds to the bathroom they are attached to
+class BathroomReview(db.Model):
+	id = db.Column(db.Integer, nullable = False)
+	BathID = db.Column(db.Integer, primary_key = True, nullable = False)
+	text = db.Column(db.Text, nullable = False)
+	cleanliness = db.Column(db.Float, nullable = False)
+	quality = db.Column(db.Float, nullable = False)
---
+++ back/test-input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import requests as req
+
+print "Inputting generic items into the database!"
+# This will run forever until you escape out
+while (True):
+	pass
